19059a33aa814c84d26d00ddc84a067ab87f6baa
szhu.spark <szhu.spark@gmail.com>
2012-10-18 11:46:58 +0000

    AliPay API

--- src/com/spshop/web/ShoppingController.java
+++ src/com/spshop/web/ShoppingController.java
@@ -32,12 +32,15 @@ import static com.spshop.utils.Constants.USER_INFO;
 import static com.spshop.utils.Constants.USER_NAME_PWD_SPLIT;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -58,6 +61,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.alipay.util.AlipayNotify;
 import com.spshop.cache.SCacheFacade;
 import com.spshop.model.Coupon;
 import com.spshop.model.Order;
@@ -531,6 +535,128 @@ public class ShoppingController extends BaseController{
 		return null;
 	}
 	
+	@RequestMapping(value="/aliPaySyncResults")
+	public String aliPaySyncResults(HttpServletRequest request, HttpServletResponse response, Model model) throws UnsupportedEncodingException{
+		//获取支付宝GET过来反馈信息
+		Map<String,String> params = new HashMap<String,String>();
+		Map requestParams = request.getParameterMap();
+		for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+			String name = (String) iter.next();
+			String[] values = (String[]) requestParams.get(name);
+			String valueStr = "";
+			for (int i = 0; i < values.length; i++) {
+				valueStr = (i == values.length - 1) ? valueStr + values[i]
+						: valueStr + values[i] + ",";
+			}
+			//乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+			valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+			params.put(name, valueStr);
+		}
+		
+		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+		//商户订单号
+
+		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+		//付款总金额
+
+		String total_fee = new String(request.getParameter("total_fee").getBytes("ISO-8859-1"),"UTF-8");
+
+		//币种
+
+		String currency = new String(request.getParameter("currency").getBytes("ISO-8859-1"),"UTF-8");
+
+		//外币金额
+		String forex_total_fee = new String(request.getParameter("forex_total_fee").getBytes("ISO-8859-1"),"UTF-8");
+
+		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+		
+		//计算得出通知验证结果
+		boolean verify_result = AlipayNotify.verify(params);
+		
+		if(verify_result){//验证成功
+			//////////////////////////////////////////////////////////////////////////////////////////
+			//请在这里加上商户的业务逻辑程序代码
+			//——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+			
+			//判断是否在商户网站中已经做过了这次通知返回的处理
+				//如果没有做过处理，那么执行商户的业务程序
+				//如果有做过处理，那么不执行商户的业务程序
+			
+			//该页面可做页面美工编辑
+			//out.println("验证成功");
+
+			//——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+			//////////////////////////////////////////////////////////////////////////////////////////
+		}else{
+			//该页面可做页面美工编辑
+			//out.println("验证失败");
+		}
+		
+		return "";
+	}
+	
+	
+	@RequestMapping(value="/aliPayAsyncResults")
+	public String aliPayAsyncResults(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException{
+		//获取支付宝POST过来反馈信息
+		PrintWriter out = response.getWriter();
+		Map<String,String> params = new HashMap<String,String>();
+		Map requestParams = request.getParameterMap();
+		for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+			String name = (String) iter.next();
+			String[] values = (String[]) requestParams.get(name);
+			String valueStr = "";
+			for (int i = 0; i < values.length; i++) {
+				valueStr = (i == values.length - 1) ? valueStr + values[i]
+						: valueStr + values[i] + ",";
+			}
+			//乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+			//valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+			params.put(name, valueStr);
+		}
+		
+		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+		//商户订单号
+
+		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+		//付款总金额
+
+		String total_fee = new String(request.getParameter("total_fee").getBytes("ISO-8859-1"),"UTF-8");
+
+		//币种
+
+		String currency = new String(request.getParameter("currency").getBytes("ISO-8859-1"),"UTF-8");
+
+		//外币金额
+		String forex_total_fee = new String(request.getParameter("forex_total_fee").getBytes("ISO-8859-1"),"UTF-8");
+
+		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+
+		if(AlipayNotify.verify(params)){//验证成功
+			//////////////////////////////////////////////////////////////////////////////////////////
+			//请在这里加上商户的业务逻辑程序代码
+
+			//——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+			
+			//判断是否在商户网站中已经做过了这次通知返回的处理
+				//如果没有做过处理，那么执行商户的业务程序
+				//如果有做过处理，那么不执行商户的业务程序
+				
+			out.println("success");	//请不要修改或删除
+
+			//——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+			//////////////////////////////////////////////////////////////////////////////////////////
+		}else{//验证失败
+			out.println("fail");
+		}
+		
+		return null;
+	}
+	
 	@RequestMapping(value="/yoursPayResults")
 	public String yoursPayResults(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		
@@ -646,6 +772,8 @@ public class ShoppingController extends BaseController{
 		
 		return rs;
 	}
+	
+	
 	
 	
 }
