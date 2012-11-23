afd7a67bcaf7eaca52ff6d68904ded5ff24c8335
realliam@gmail.com <realliam@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2012-11-23 09:20:28 +0000

    pagination on tag products page

--- trunk/src/com/spshop/web/TagsController.java
+++ trunk/src/com/spshop/web/TagsController.java
@@ -2,7 +2,6 @@ package com.spshop.web;
 
 import static com.spshop.utils.Constants.CURRENT_INDEX_KEY_LIST;
 import static com.spshop.utils.Constants.CURRENT_INDEX_KEY_PAGE;
-import static com.spshop.utils.Constants.CURRENT_INDEX_KEY_PAGE_COUNT;
 import static com.spshop.utils.Constants.CURRENT_KEYWORD;
 import static com.spshop.utils.Constants.CURRENT_KEYWORD_2;
 import static com.spshop.utils.Constants.IMAGE_SIZE_INFO_KEY;
@@ -33,6 +32,7 @@ public class TagsController extends BaseController{
 	public static int KEYWORD_PER_PAGE = 120;
 	private static final int MAX_PAGE_INDEXES_DISPLAY = 5;
     private static final String KEYWORD_COUNT = "keywordsCount";
+    private static final String PRODUCTS_COUNT = "productsCount";
     private static String PAGE_INDEX = "pageIndex";
     private static String MAX_PAGE_NUM = "maxPageNum";
     private static String START_INDEX = "startIndex";
@@ -100,31 +100,55 @@ public class TagsController extends BaseController{
 	
 
 	@RequestMapping("/k/{keyword}/{page}")
-	public String tagKey(Model model, @PathVariable("keyword")String key,  @PathVariable("page")int page){
+	public String tagKey(Model model, @PathVariable("keyword")String key,  @PathVariable("page")int pageNum){
 		
 		 TabProduct topSelling = SCacheFacade.getTopSelling(0,false);
 		 
-		 List<Product> products = SCacheFacade.getTagsProducts(key.replace('-', ' '), page);
+		 List<Product> products = SCacheFacade.getTagsProducts(key.replace('-', ' '), pageNum);
+         String kw = key.replace('-', ' ');
 		 
 		 if(null != products){
-			 model.addAttribute(CURRENT_INDEX_KEY_PAGE, page);
-			 
-			 model.addAttribute(TOP_SELLING_TAB_PRODUCT, topSelling);
-			 
-			 String kw = key.replace('-', ' ');
-			 
-			 model.addAttribute(CURRENT_KEYWORD_2, kw);
-			 
-			 model.addAttribute(TAGED_PRODUCTS, products);
-			 
-			 int count = ServiceFactory.getService(ProductService.class).getProductCountByTag(kw);
-			 
-			 List<Float> imageSize = Utils.figureOutProductsSize(products);
-			 
-			 model.addAttribute(IMAGE_SIZE_INFO_KEY , imageSize);
-			 
-			 model.addAttribute(CURRENT_INDEX_KEY_PAGE_COUNT, count%TAGS_PRODUCT_PER_PAGE==0?count/TAGS_PRODUCT_PER_PAGE:count/TAGS_PRODUCT_PER_PAGE+1);
-		 }
+	            model.addAttribute(CURRENT_INDEX_KEY_PAGE, pageNum);
+	            model.addAttribute(TOP_SELLING_TAB_PRODUCT, topSelling);
+	            model.addAttribute(TAGED_PRODUCTS, products);
+	            
+	            int count = ServiceFactory.getService(ProductService.class).getProductCountByTag(kw);
+	            List<Integer> pageIndexes = new ArrayList<Integer>();
+	            int maxPageNum = (int)((count-1)/TAGS_PRODUCT_PER_PAGE+1);
+	            if (maxPageNum >= MAX_PAGE_INDEXES_DISPLAY) {
+	                int rTempIdx = pageNum, lTempIdx = pageNum;
+	                pageIndexes.add(pageNum);
+	                while (pageIndexes.size()<MAX_PAGE_INDEXES_DISPLAY) {
+	                    if (rTempIdx + 1 <= maxPageNum) {
+	                        rTempIdx++;
+	                        pageIndexes.add(rTempIdx);
+	                    }
+	                    if (lTempIdx - 1 > 0) {
+	                        lTempIdx--;
+	                        pageIndexes.add(lTempIdx);
+	                    }
+	                }
+	                Collections.sort(pageIndexes);
+	            } else {
+	                for (int i = 1; i < maxPageNum; i++) {
+	                    pageIndexes.add(i);
+	                }
+	            }
+	            if (!pageIndexes.contains(maxPageNum)) {
+	                model.addAttribute(MAX_PAGE_NUM, maxPageNum);
+	            }
+	            if (!pageIndexes.contains(1)) {
+	                model.addAttribute(FIRST_PAGE_INDEX, 1);
+	            }
+	            List<Float> imageSize = Utils.figureOutProductsSize(products);
+	            model.addAttribute(IMAGE_SIZE_INFO_KEY , imageSize);
+	            model.addAttribute(CURRENT_KEYWORD_2, kw);
+	            model.addAttribute(PAGE_INDEX, pageIndexes);
+	            model.addAttribute(PRODUCTS_COUNT, count);
+	            model.addAttribute(START_INDEX, KEYWORD_PER_PAGE * (pageNum - 1) + 1);
+	            model.addAttribute(END_INDEX, KEYWORD_PER_PAGE * (pageNum - 1) + products.size());
+	            model.addAttribute(PAGE_NUM, pageNum);
+	        }
 		 
 		 return "/tags/tagKeyword";
 	}
