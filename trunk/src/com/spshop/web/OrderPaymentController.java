c8c093a119d378004962d36d09c89ea2184d46ae
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2014-01-09 04:56:15 +0000

    Alipay

--- trunk/src/com/spshop/web/OrderPaymentController.java
+++ trunk/src/com/spshop/web/OrderPaymentController.java
@@ -4,6 +4,7 @@ import static com.spshop.utils.Constants.DEFAULT_CURRENCY;
 import static com.spshop.utils.Constants.SHOPPINGCART;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alipay.config.AlipayConfig;
+import com.alipay.util.AlipaySubmit;
 import com.spshop.model.Country;
 import com.spshop.model.Coupon;
 import com.spshop.model.Order;
@@ -193,7 +196,7 @@ public class OrderPaymentController extends BaseController{
 	}
 	
 	@RequestMapping("/checkout")
-	public String checkout( Model model, HttpServletRequest request, @RequestParam("orderSN")String orderSN,  @RequestParam("payment")String payment){
+	public String checkout( Model model, HttpServletRequest request, @RequestParam("orderSN")String orderSN,  @RequestParam("payment")String payment) throws Exception{
 		
 		Order order = ServiceFactory.getService(OrderService.class).getCartOrPendingOrderById(orderSN, getUserView().getLoginUser().getId());
 		model.addAttribute(Constants.PROCESSING_ORDER, order);
@@ -213,6 +216,80 @@ public class OrderPaymentController extends BaseController{
 				order.setOrderType("Paypal");
 				checkOutOrder(request, model, order);
 				return "paypal";
+			}else if("alipay".equals(payment)) {
+				order.setOrderType("Alipay");
+				checkOutOrder(request, model, order);
+				
+				Map<String,String> sParaTemp = new HashMap<String, String>();
+				
+				////////////////////////////////////请求参数//////////////////////////////////////
+				
+				//支付类型
+				String payment_type = "1";
+				//必填，不能修改
+				//服务器异步通知页面路径
+				String notify_url = "http://"+request.getServerName()+"/uc/alipayRs";
+				//需http://格式的完整路径，不能加?id=123这类自定义参数
+				
+				//页面跳转同步通知页面路径
+				String return_url = "http://"+request.getServerName()+"/uc/alipayReturn";
+				//需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
+				
+				//卖家支付宝帐户
+				String seller_email = new String(AlipayConfig.ali_account);
+				//必填
+				
+				//商户订单号
+				String out_trade_no = new String(order.getName());
+				//商户网站订单系统中唯一订单号，必填
+				
+				//订单名称
+				String subject = new String(order.getName());
+				//必填
+				
+				//付款金额
+				String total_fee = new String(
+						new NumberFormat("##0.##").getNumberFormat().format(getSiteView().getCurrencies().get(order.getCurrency())*(order.getTotalPrice() + order.getDePrice() - order.getCouponCutOff()))
+						);
+				//必填
+				
+				//订单描述
+				
+				/*String body = new String(request.getParameter("WIDbody").getBytes("ISO-8859-1"),"UTF-8");*/
+				//商品展示地址
+				/*String show_url = new String(request.getParameter("WIDshow_url").getBytes("ISO-8859-1"),"UTF-8");*/
+				//需以http://开头的完整路径，例如：http://www.xxx.com/myorder.html
+				
+				//防钓鱼时间戳
+				/*String anti_phishing_key = "";*/
+				//若要使用请调用类文件submit中的query_timestamp函数
+				
+				//客户端的IP地址
+				String exter_invoke_ip = "";
+				//非局域网的外网IP地址，如：221.0.0.1
+				
+				sParaTemp.put("service", "create_direct_pay_by_user");
+		        sParaTemp.put("partner", AlipayConfig.partner);
+		        sParaTemp.put("_input_charset", AlipayConfig.input_charset);
+				sParaTemp.put("payment_type", payment_type);
+				sParaTemp.put("notify_url", notify_url);
+				sParaTemp.put("return_url", return_url);
+				sParaTemp.put("seller_email", seller_email);
+				sParaTemp.put("out_trade_no", out_trade_no);
+				sParaTemp.put("subject", subject);
+				sParaTemp.put("total_fee", total_fee);
+				/*sParaTemp.put("body", body);
+				sParaTemp.put("show_url", show_url);*/
+				/*sParaTemp.put("anti_phishing_key", anti_phishing_key);*/
+				sParaTemp.put("exter_invoke_ip", exter_invoke_ip);
+				
+				/*sParaTemp.put("service", "alipay.trade.direct.forcard.pay");*/
+				
+				String formRequest = AlipaySubmit.buildRequest(sParaTemp, "POST", "");
+				
+				model.addAttribute("formRequest", formRequest);
+				
+				return "alipay";
 			}else if("WesternUnion".equals(payment)) {
 				order.setOrderType("WesternUnion");
 				checkOutOrder(request, model, order);
