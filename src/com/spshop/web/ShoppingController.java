c72ce5b890c94be168c39222949e74d5d0e60cd8
szhu.spark <szhu.spark@gmail.com>
2014-01-09 07:08:09 +0000

    Alipay

--- src/com/spshop/web/ShoppingController.java
+++ src/com/spshop/web/ShoppingController.java
@@ -694,8 +694,6 @@ public class ShoppingController extends BaseController{
 	@RequestMapping("/alipayReturn")
 	public String alipayReturn(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException{
 		
-		PrintWriter out = response.getWriter();
-		//获取支付宝GET过来反馈信息
 		Map<String,String> params = new HashMap<String,String>();
 		Map requestParams = request.getParameterMap();
 		for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
@@ -712,16 +710,14 @@ public class ShoppingController extends BaseController{
 		}
 		
 		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
-		//商户订单号
-
+		//商户网站唯一订单号
 		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+		//对应商户网站的订单系统中的唯一订单号
 
-		//支付宝交易号
-
-		String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
 
 		//交易状态
 		String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+		//交易目前所处的状态(例如：TRADE_SUCCESS)
 
 		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
 		
@@ -729,15 +725,6 @@ public class ShoppingController extends BaseController{
 		boolean verify_result = AlipayNotify.verify(params);
 		
 		if(verify_result){//验证成功
-			//////////////////////////////////////////////////////////////////////////////////////////
-			//请在这里加上商户的业务逻辑程序代码
-
-			//——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
-			if(trade_status.equals("TRADE_FINISHED") || trade_status.equals("TRADE_SUCCESS")){
-				//判断该笔订单是否在商户网站中已经做过处理
-					//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-					//如果有做过处理，不执行商户的业务程序
-			}
 			
 			//该页面可做页面美工编辑
 			//out.println("验证成功<br />");
@@ -775,43 +762,18 @@ public class ShoppingController extends BaseController{
 		}
 		
 		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
-		//商户订单号
-
+		//商户网站唯一订单号
 		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+		//对应商户网站的订单系统中的唯一订单号
 
-		//支付宝交易号
-
-		String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
 
 		//交易状态
 		String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+		//交易目前所处的状态(例如：TRADE_SUCCESS)
 
 		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
 
 		if(AlipayNotify.verify(params)){//验证成功
-			//////////////////////////////////////////////////////////////////////////////////////////
-			//请在这里加上商户的业务逻辑程序代码
-
-			//——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
-			
-			if(trade_status.equals("TRADE_FINISHED")){
-				//判断该笔订单是否在商户网站中已经做过处理
-					//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-					//如果有做过处理，不执行商户的业务程序
-					
-				//注意：
-				//该种交易状态只在两种情况下出现
-				//1、开通了普通即时到账，买家付款成功后。
-				//2、开通了高级即时到账，从该笔交易成功时间算起，过了签约时的可退款时限（如：三个月以内可退款、一年以内可退款等）后。
-			} else if (trade_status.equals("TRADE_SUCCESS")){
-				//判断该笔订单是否在商户网站中已经做过处理
-					//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-					//如果有做过处理，不执行商户的业务程序
-					
-				//注意：
-				//该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
-			}
-
 			//——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
 			
 			Order order = ServiceFactory.getService(OrderService.class).getOrderById(out_trade_no);
